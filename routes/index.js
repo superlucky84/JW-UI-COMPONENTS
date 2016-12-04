78f876e619c91929f985365dd0a8dfd4e49a3fc9
kimjinwoo <superlucky84@gmail.com>
2016-12-04 21:03:07 +0900

    * CHANGED port and index page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('vanilla/scroll', { title: 'Express' });
 });
 
 module.exports = router;
